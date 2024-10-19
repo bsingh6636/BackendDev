7b36bcd0b249b64152921cadaea0f92540c79176
Brijesh Singh <bsingh6636@gmail.com>
2024-10-19 14:52:31 +0530

    email change

--- portfolio/controller/Model/FormController.js
+++ portfolio/controller/Model/FormController.js
@@ -1,28 +1,29 @@
 
-import nodemailer from 'nodemailer'; 
+import nodemailer from 'nodemailer';
 export const SendformData = (req, res) => {
-    const { name, email, message } = req.body;
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-    const textMessage = `From: ${email}\tMessage:\t${message}`;
-    console.log(textMessage)
-    const mailOptions = {
-      from: email,
-      to: 'bsingh6636@outlook.com',
-      subject: `New message from ${name}`,
-      text: textMessage
-    };
-  
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log(error);
-        return res.json({ status: 'error',error });
-      }
-      res.json({ status: 'success' });
-    });
-  }
+  console.log('hi')
+  const { name, email, message } = req.body;
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+  const textMessage = `From: ${email}\tMessage:\t${message}`;
+  console.log(textMessage)
+  const mailOptions = {
+    from: email,
+    to: 'bkushwaha.dev@gmail.com',
+    subject: `New message from ${name}`,
+    text: textMessage
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      return res.json({ status: 'error', error });
+    }
+    res.json({ status: 'success' });
+  });
+}
